7506da3b2cb887e5e51a61f2097799637c2a6a8f
unknown <marcellui.ca@gmail.comgit config --global user.name marcellui>
2023-10-31 14:35:29 -0400

    update routes get requests

--- routes/videos.js
+++ routes/videos.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-console.log("hello");
+router.get("/videos", (req,res) => {
+    res.send(videos)
+})
+
+router.get("/videos/:id", (req,res)=> {
+    const foundVideo = videos.find(video => video.id === req.params.id)
+    res.json(foundVideo)
+})
+
+module.exports = router;
