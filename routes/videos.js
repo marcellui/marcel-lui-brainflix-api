e439988b1a51b8a03e355c493af79ba064d5671b
unknown <marcellui.ca@gmail.comgit config --global user.name marcellui>
2023-10-31 18:08:27 -0400

    update routes

--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
-router.get("/videos", (req,res) => {
-    res.send(videos)
+
+const tempVideos  = fs.readFileSync("./data/videos.json");
+const videosData = JSON.parse(tempVideos);
+
+router.get("/", (req,res) => {
+    res.send(videosData)
 })
 
-router.get("/videos/:id", (req,res)=> {
-    const foundVideo = videos.find(video => video.id === req.params.id)
+router.get("/:id", (req,res)=> {
+    const foundVideo = videosData.find(video => video.id === req.params.id)
     res.json(foundVideo)
 })
 
+router.post("/", (req,res)=> {
+    const {title, description} = req.body;
+    const newVideo ={
+        id: uuidv4(),
+        title,
+        description,
+        channel,
+        image,
+        views,
+        likes,
+        duration,
+        video,
+        timestamp,
+        comments
+    }
+})
 module.exports = router;
